aa08d5e61beff659c0c8a834368f4b7dbb83d2cd
mestroedge <98157564+mestroedge@users.noreply.github.com>
2022-02-20 16:06:10 +0530

    factors

---
+++ factors.java
@@ -0,0 +1,16 @@
+
+public class factors {
+public static void main (String args[])
+{
+	int N = args[0];
+	System.out.println("welcome");
+	for (int i=2;i*i<=N;i+4);
+	if (N%i==0);
+	{
+while (N%i==0);
+{
+	System.out.println(i);
+	N = N/i;
+	}
+}
+
