3d868f3fb179bf51f12113067b839c5a577bbead
mestroedge <98157564+mestroedge@users.noreply.github.com>
2022-02-20 16:02:52 +0530

    powerof2

---
+++ Power.java
@@ -0,0 +1,12 @@
+package powerof2;
+
+public class Power {
+ public static void main (String args[])
+ {
+	 int N = args[]
+			 for (int i=20;i<N;i++)
+			 {
+				 System.out.println (2^i);
+			 }
+ }
+}
